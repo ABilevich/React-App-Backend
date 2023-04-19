7412d3e27e9ef64d531fc586d7a79c2b9b02a70b
ABilevich <andresbilevich@gmail.com>
2023-04-19 11:19:11 +0200

    updated variable name

--- app.js
+++ app.js
@@ -13,9 +13,11 @@ app.listen(process.env.PORT, () => {
 });
 
 app.post("/signature", jsonParser, (req, res) => {
-  const { username, pin } = req.body;
-  if (!username || username == "")
-    res.status(400).json({ message: "Bad Request: The username is invalid" });
+  const { name, pin } = req.body;
+  if (!pin || !name || name == "")
+    res
+      .status(400)
+      .json({ message: "Bad Request: The name or pin is invalid" });
   if (pin == process.env.PIN) {
     res.status(200).json({ message: "Success: PDF Signed" });
   } else {
