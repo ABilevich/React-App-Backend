a98323265ac75b3bc547af06dc83636c31e70306
ABilevich <andresbilevich@gmail.com>
2023-04-19 09:52:49 +0200

    Initial commit for upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SproofChallengeBackend
+
 This repo contains the backend from the Sproof challenge
+
+To run:
+
+### `npm install`
+
+### `node .\app.js`
---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+var app = express();
+app.use(cors());
+
+var jsonParser = bodyParser.json();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
+
+app.post("/signature", jsonParser, (req, res) => {
+  const { username, pin } = req.body;
+  console.log(req.body);
+  if (!username || username == "")
+    res.status(400).json({ message: "Bad Request: The username is invalid" });
+  if (pin == process.env.PIN) {
+    res.status(200).json({ message: "Success: PDF Signed" });
+  } else {
+    res.status(401).json({ message: "Error: Wrong PIN" });
+  }
+});
